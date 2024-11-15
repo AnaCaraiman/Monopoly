51ba78c8b20c6e4102b69896955afa686e5b849b
omacelaru <octavian-andrei.macelaru@s.unibuc.ro>
2024-11-15 13:55:06 +0200

    [DEV] - Starting the rollDice method

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,40 +1,33 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-
     [SerializeField] private MonopolyBoard gameBoard;
     [SerializeField] private List<Player> playerList = new List<Player>();
     [SerializeField] private int currentPlayerIndex;
 
     [Header("Globla Game Settings")] [SerializeField]
     private int maxTurnsInJail = 3;
-
     [SerializeField] private int startMoney = 1500;
     [SerializeField] private int goMoney = 500;
 
     [Header("Player Info")] [SerializeField]
     GameObject playerInfoPrefab;
-
     [SerializeField] private Transform playerPanel; // Where the player info will be displayed
     [SerializeField] private List<GameObject> playerTokenList = new List<GameObject>();
 
+    private int[] rolledDice;
+    private bool rolledADouble;
+    private int doubleRollCount;
+    
+
     void Awake()
     {
-        // if(instance == null)
-        // {
         instance = this;
-        // }
-        // else
-        // {
-        //     Destroy(this);
-        // }
     }
 
     private void Start()
@@ -56,5 +49,19 @@ public class GameManager : MonoBehaviour
                 Quaternion.identity);
             playerList[i].InitializePlayer(gameBoard.route[0], startMoney, playerInfoComponent, newToken);
         }
+    }
+
+    public void RollDice()
+    {
+        rolledDice = new int[2];
+        rolledDice[0] = Random.Range(1, 7);
+        rolledDice[1] = Random.Range(1, 7);
+
+        rolledADouble = rolledDice[0] == rolledDice[1];
+    }
+
+    IEnumerator DelayBeforMove()
+    {
+        yield return new WaitForSeconds(2f);
     }
 }
