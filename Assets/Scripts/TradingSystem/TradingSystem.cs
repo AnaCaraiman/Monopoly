025c787c42e7e7ad80ccb6bbed6991aaadba5177
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2025-01-07 16:47:34 +0200

    Trading Player Panels

--- Assets/Scripts/TradingSystem/TradingSystem.cs
+++ Assets/Scripts/TradingSystem/TradingSystem.cs
@@ -3,9 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using System;
+using TMPro;
+using UnityEngine.UI;
 public class TradingSystem : MonoBehaviour
 {
     public static TradingSystem instance;
+
+    [SerializeField] GameObject cardPrefab;
+    [SerializeField] GameObject tradePanel;
+
+    [Header("LEFT SIDE")]
+    [SerializeField] TMP_Text leftOffererNameText;
+    [SerializeField] Transform leftCardGrid;
+    [SerializeField] ToggleGroup leftToggleGroup;
+    [SerializeField] TMP_Text leftYourMoneyText;
+    [SerializeField] TMP_Text leftOfferMoney;
+    [SerializeField] Slider leftMoneySlider;
+    List<GameObject> leftCardPrefabList = new List<GameObject>();
+    int leftChosenMoneyAmount;
+    MonopolyNode leftSelectedNode;
+    Player leftPlayerReference;
+
+    [Header("MIDDLE")]
+    [SerializeField] Transform buttonGrid;
+    [SerializeField] GameObject playerButtonPrefab;
+
+    List<GameObject> playerButtonList = new List<GameObject>();
+
+    [Header("RIGHT SIDE")]
+    [SerializeField] TMP_Text rightOffererNameText;
+    [SerializeField] Transform rightCardGrid;
+    [SerializeField] ToggleGroup rightToggleGroup;
+    [SerializeField] TMP_Text rightYourMoneyText;
+    [SerializeField] TMP_Text rightOfferMoney;
+    [SerializeField] Slider rightMoneySlider;
+    List<GameObject> rightCardPrefabList = new List<GameObject>();
+    int rightChosenMoneyAmount;
+    MonopolyNode rightSelectedNode;
+    Player rightPlayerReference;
 
     //MESSAGE SYSTEM
     public delegate void UpdateMessage(string message);
@@ -13,6 +48,10 @@ public class TradingSystem : MonoBehaviour
     void Awake() 
     {
         instance = this;
+    }
+    void Start()
+    {
+        tradePanel.SetActive(false);
     }
 
     public void FindMissingProperty(Player currentPlayer)
@@ -174,5 +213,139 @@ public class TradingSystem : MonoBehaviour
                 OnUpdateMessage.Invoke(currentPlayer.name + " sold " + offeredNode.name +  " to " + nodeOwner.name + " for" + requestedMoney);
             }
         
+    }
+
+    void CreateLeftPanel()
+    {
+        leftOffererNameText.text = leftPlayerReference.name;
+
+        for(int i = 0; i< leftPlayerReference.GetMonopolyNodes.Count; i++)
+        {
+            GameObject tradeCard = Instantiate(cardPrefab, leftCardGrid, false);
+            leftCardPrefabList.Add(tradeCard);
+        }
+        leftYourMoneyText.text = "Your Money: " + leftPlayerReference.ReadMoney;
+
+        leftMoneySlider.maxValue = leftPlayerReference.ReadMoney;
+
+        leftMoneySlider.value = 0;
+        UpdateLeftSlider(leftMoneySlider.value);
+
+
+        tradePanel.SetActive(true);
+    }
+
+    public void UpdateLeftSlider(float value)
+    {
+        leftOfferMoney.text = "Offer money: $ " + leftMoneySlider.value;
+
+    }
+
+    public void CloseTradePanel()
+    {
+        tradePanel.SetActive(false);
+
+        ClearAll();
+    }
+
+    public void OpenTradePanel()
+    {
+        leftPlayerReference = GameManager.instance.GetCurrentPlayer;
+        rightOffererNameText.text = "Select a Player";
+
+        CreateLeftPanel();
+
+        CreateMiddleButton();
+    }
+
+
+    public void ShowRightPlayer(Player player)
+    {
+        rightPlayerReference = player;
+        ClearRightPanel();
+
+        rightOffererNameText.text = rightPlayerReference.name;
+
+        for(int i = 0; i< rightPlayerReference.GetMonopolyNodes.Count; i++)
+        {
+            GameObject tradeCard = Instantiate(cardPrefab, rightCardGrid, false);
+            rightCardPrefabList.Add(tradeCard);
+        }
+        rightYourMoneyText.text = "Your Money: " + rightPlayerReference.ReadMoney;
+
+        rightMoneySlider.maxValue = rightPlayerReference.ReadMoney;
+
+        rightMoneySlider.value = 0;
+        UpdateRightSlider(rightMoneySlider.value);
+
+
+        tradePanel.SetActive(true);
+    }
+
+    void CreateMiddleButton()
+    {
+        for(int i = playerButtonList.Count - 1; i >=0 ; i-- )
+        {
+            Destroy(playerButtonList[i]);
+        }
+
+        playerButtonList.Clear();
+
+        List<Player> allPlayers = new List<Player>();
+        allPlayers.AddRange(GameManager.instance.GetPlayers);
+        allPlayers.Remove(leftPlayerReference);
+
+        foreach(var player in allPlayers)
+        {
+            GameObject newPlayerButton = Instantiate(playerButtonPrefab, buttonGrid, false);
+            newPlayerButton.GetComponent<TradePlayerButton>().SetPlayer(player);
+
+            playerButtonList.Add(newPlayerButton);
+        }
+
+    }
+
+    void ClearAll()
+    {
+        rightOffererNameText.text = "Select a Player";
+        rightYourMoneyText.text = "Your Money: $ 0";
+        rightMoneySlider.maxValue = 0;
+        rightMoneySlider.value = 0;
+        UpdateRightSlider(rightMoneySlider.value);
+        for(int i = playerButtonList.Count - 1; i >=0 ; i-- )
+        {
+            Destroy(playerButtonList[i]);
+        }
+
+        for(int i = leftCardPrefabList.Count - 1; i >=0 ; i-- )
+        {
+            Destroy(leftCardPrefabList[i]);
+        }
+
+        for(int i = rightCardPrefabList.Count - 1; i >=0 ; i-- )
+        {
+            Destroy(rightCardPrefabList[i]);
+        }
+    }
+
+    void ClearRightPanel()
+    {  
+        for(int i = rightCardPrefabList.Count - 1; i >=0 ; i-- )
+        {
+            Destroy(rightCardPrefabList[i]);
+        }
+
+        rightCardPrefabList.Clear(); 
+
+        rightMoneySlider.maxValue = 0;
+
+        rightMoneySlider.value = 0;
+        UpdateRightSlider(rightMoneySlider.value);   
+    }
+
+    public void UpdateRightSlider(float value)
+    {
+        rightOfferMoney.text = "Requested money: $ " + rightMoneySlider.value;
+
     }
 }
