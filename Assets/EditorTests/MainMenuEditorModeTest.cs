e5004da31fd9a297da1d6bed6182b07fc7ea6129
Macelaru Octavian <octavian-andrei.macelaru@s.unibuc.ro>
2025-02-01 17:29:58 +0200

    [DEV] - Added test for main menu play mode

---
+++ Assets/EditorTests/MainMenuEditorModeTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+using TMPro;
+public class MainMenuEditorModeTest
+{
+    
+}
